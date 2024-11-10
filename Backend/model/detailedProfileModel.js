f86bdc906e325ca8cd9f0a260bad892ce63507f0
Hemal Ravrani <202201235@daiict.ac.in>
2024-11-11 01:12:24 +0530

    Updated detailedProfileModel.js

--- Backend/model/detailedProfileModel.js
+++ Backend/model/detailedProfileModel.js
@@ -2,29 +2,41 @@
 const mongoose = require('mongoose');
 
 const detailedProfileSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    basicProfileId: { type: mongoose.Schema.Types.ObjectId, ref: 'BasicProfile', required: true },
-    information: {
-        phoneNumber: { type: String, match: /^[0-9]{10}$/ },
+    name: { type: String, required: true, immutable: true },
+    id: { type: String, unique: true, required: true, immutable: true },
+    role: { type: String, immutable: true },
+    profileImage: { type: String, required: true},
+
+    basicInfo: {
+        firstName: {type: String},
+        lastName: {type: String},
+        id: { type: String, unique: true, required: true },
         fatherName: { type: String, trim: true },
         motherName: { type: String, trim: true },
-        aadharNumber: { type: String, unique: true, match: /^[0-9]{12}$/ },
-        mailId: { type: String, unique: true, trim: true, match: /\S+@\S+\.\S+/ },
         birthDate: { type: Date },
-        address: {
-            city: { type: String, trim: true },
-            district: { type: String, trim: true },
-            taluka: { type: String, trim: true },
-            country: { type: String, trim: true, default: 'India' }
-        },
+        mail: { type: String, unique: true, trim: true, match: /\S+@\S+\.\S+/ },
+        phoneNumber: { type: String, match: /^[0-9]{10}$/ },
+        gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+        role: {type: String},
         nationality: { type: String, trim: true, default: 'Indian' },
         religion: { type: String, trim: true },
-        emergencyContact: { type: String, match: /^[0-9]{10}$/ },
-        bloodGroup: { type: String, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] },
-        dateOfJoining: { type: Date },
-        gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true }
     },
-    backDetails: { type: String, trim: true }
+    address: {
+        block: { type: String, trim: true },
+        street: { type: String, trim: true },
+        village: { type: String, trim: true },
+        taluka: { type: String, trim: true },
+        district: { type: String, trim: true },
+        pincode: { type: String, trim: true, match: /^[0-9]{6}$/ },
+        country: { type: String, trim: true, default: 'India' },
+    },
+    bankDetails: { 
+        bankName: {type: String, trim: true, required: true}, 
+        ifscCode: {type: String, trim: true, required: true}, 
+        accountNo: {type: String, trim: true, required: true}, 
+        aadharNumber: { type: String, unique: true, match: /^[0-9]{12}$/ },
+    },
 });
 
-module.exports = mongoose.model('DetailedProfile', detailedProfileSchema);
+const DetailedProfile = mongoose.model('DetailedProfile', detailedProfileSchema);
+module.exports = DetailedProfile;
